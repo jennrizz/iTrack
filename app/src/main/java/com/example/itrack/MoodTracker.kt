265ccb073767152f86bd431742003f469776d31d
Joshie <kalilinux19701375@gmail.com>
2021-03-08 11:48:21 +0800

    new

---
+++ app/src/main/java/com/example/itrack/MoodTracker.kt
@@ -0,0 +1,11 @@
+package com.example.itrack
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MoodTracker : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mood_tracker)
+    }
+}
